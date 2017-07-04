2fd288eb16d4a05e1fb5cd2866651c949c1b6d68
Brandon Dean <bdean@ikayzo.com>
2017-07-03 15:22:55 -1000

    Start of Part 4 - create AuthService

--- angular-token-auth/src/app/app.module.ts
+++ angular-token-auth/src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { BrowserModule }        from '@angular/platform-browser';
-import { FormsModule }          from '@angular/forms';
-import { HttpModule }           from '@angular/http';
-import { NgModule }             from '@angular/core';
-import { MaterializeModule }    from 'angular2-materialize';
+// Modules
+import { BrowserModule }         from '@angular/platform-browser';
+import { FormsModule }           from '@angular/forms';
+import { HttpModule }            from '@angular/http';
+import { NgModule }              from '@angular/core';
+import { MaterializeModule }     from 'angular2-materialize';
+import { AppRoutingModule }      from './app-routing.module';
 
-import { AppRoutingModule }     from './app-routing.module';
-import { AppComponent }         from './app.component';
-
-import { Angular2TokenService } from 'angular2-token';
-import { HomeComponent } from './home/home.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { AuthDialogComponent } from './auth-dialog/auth-dialog.component';
-import { LoginFormComponent } from './login-form/login-form.component';
+// Components
+import { AppComponent }          from './app.component';
+import { HomeComponent }         from './home/home.component';
+import { ToolbarComponent }      from './toolbar/toolbar.component';
+import { AuthDialogComponent }   from './auth-dialog/auth-dialog.component';
+import { LoginFormComponent }    from './login-form/login-form.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
+
+// Services
+import { Angular2TokenService }  from 'angular2-token';
+import { AuthService }             from "./services/auth.service";
 
 @NgModule({
   declarations: [
@@ -30,7 +34,7 @@ import { RegisterFormComponent } from './register-form/register-form.component';
     AppRoutingModule,
     MaterializeModule
   ],
-  providers: [Angular2TokenService],
+  providers: [Angular2TokenService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
