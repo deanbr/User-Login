37b89453ecdc661d3d7a3f9cc07cda512066cc68
Brandon Dean <bdean@ikayzo.com>
2017-07-03 14:48:33 -1000

    Updated AuthDialog with login/ registration

--- angular-token-auth/src/app/auth-dialog/auth-dialog.component.ts
+++ angular-token-auth/src/app/auth-dialog/auth-dialog.component.ts
@@ -16,11 +16,34 @@ export class AuthDialogComponent implements OnInit {
   ngOnInit() {
   }
 
+  // Event handlers
+  onLoginFormResult(e){
+    if(e.signedIn)
+      this.closeDialog();
+    else{
+      alert(e.err.json().errors[0])
+    }
+  }
+
+  onRegisterFormResult(e){
+    if(e.signedUp)
+      this.closeDialog();
+    else{
+      alert(e.err.json().errors.full_messages[0])
+    }
+  }
+
+  // General Open / Close actions
   openDialog(mode: 'login' | 'register' = 'login'){
     this.authMode = mode;
     this.modalActions.emit({action:"modal", params:['open']});
   }
 
+  closeDialog(){
+    this.modalActions.emit({action:"modal", params:['close']});
+  }
+
+  // Abstracted basic logic
   isLoginMode(){ return this.authMode == 'login' }
   isRegisterMode(){ return this.authMode == 'register' }
 
